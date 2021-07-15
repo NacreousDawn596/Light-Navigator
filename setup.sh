ed3e5a10acee6b9f741606289689558d0123195a
NacreousDawn596 <75474839+NacreousDawn596@users.noreply.github.com>
2021-07-15 23:19:22 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,1 +1,1 @@
-cd && cd 'Light Navigator' && sudo apt-get install nodejs python3 figlet && sudo npm install electron -g && npm install electron --save-dev && python3 navigator.py && rm setup.sh
+cd && cd Light-Navigator && sudo apt-get install nodejs python3 figlet && sudo npm install electron -g && npm install electron --save-dev && python3 navigator.py && rm setup.sh
