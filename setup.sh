5e250a9a89b466bc1eca4716e862c9a04fec27c9
NacreousDawn596 <75474839+NacreousDawn596@users.noreply.github.com>
2021-07-16 18:42:28 +0530

    Updated setup.sh
    Code's a bit cleaner and added support for all distros!

--- setup.sh
+++ setup.sh
@@ -1,1 +1,46 @@
-cd && cd Light-Navigator && sudo apt-get install nodejs python3 figlet && sudo npm install electron -g && npm install electron --save-dev && python3 navigator.py && rm setup.sh
+cd && cd Lowser && sudo apt-get install nodejs python3 figlet && sudo npm install electron -g && npm install electron --save-dev && python3 navigator.py && rm setup.sh
+
+#!/bin/sh
+
+cd 
+cd Lowser
+
+sudo pacman -S cowsay
+sudo apt-get install cowsay
+sudo dnf install cowsay
+
+cowsay Choose your linux distrobution :
+    echo [1] Debian
+    echo [2] Arch
+    echo [3] Fedora
+echo type your choice :
+read -r Distobution
+    
+    function Arch()
+    { 
+    echo "installing for arch"
+    sudo pacman -S nodejs
+    sudo pacman -S python3
+    sudo pacman -S figlet
+    }
+    function Debian()
+    {
+    echo "installing for debian"
+    sudo apt-get install nodejs
+    sudo apt-get install figlet
+    sudo apt-get install python3
+    }
+    function Fedora ()
+    {
+    sudo dnf install python3
+    sudo dnf install nodejs
+    sudo dnf install figlet
+    }
+    if [[ "$Distobution" == 1 ]]; then
+      Debian
+    elif [[ "$Distobution" == 2 ]]; then
+      Arch
+    elif [[ "$Distobution" == 3 ]]; then
+      Fedora
+    
+    
