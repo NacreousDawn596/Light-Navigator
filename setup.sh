d8429c5b8fa0824b40c1dd2f6819b4a6c5990fe9
NacreousDawn596 <75474839+NacreousDawn596@users.noreply.github.com>
2021-07-16 18:47:19 +0530

    Updated setup.sh
    Fixed a bug

--- setup.sh
+++ setup.sh
@@ -1,5 +1,3 @@
-cd && cd Lowser && sudo apt-get install nodejs python3 figlet && sudo npm install electron -g && npm install electron --save-dev && python3 navigator.py && rm setup.sh
-
 #!/bin/sh
 
 cd 
