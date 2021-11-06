18b675d8b763da8a720ca9b281544476708a3d2f
NacreousDawn596 <75474839+NacreousDawn596@users.noreply.github.com>
2021-11-06 15:03:09 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,33 @@
 import os
-
+import sys
+from requests import *
+from bs4 import *
+def makesure(link):
+	if get(link).status_code == 200:
+		security(link)
+	else:
+		print("link incorrect")
+		sys.exit()
+def security(ty):
+	result = BeautifulSoup(os.popen(f"curl -s http://trueurl.net/service?q={ty.replace(':', '%3A').replace('/', '%2F')}&lucky=on&Uncloak=Find+True+URL").read(), 'html.parser').find_all('td')[3].find_all('a')
+	try:
+		if len(result) == 1:
+			result = result[0].get('href')
+		else:
+			result = result[1].get('href')
+		if ty == result:
+			pass
+		else:
+			umm = input(f"do you want to be redirected to this link: '{result}'? y/n\n->")
+			if "y" in umm.lower():
+				pass
+			else:
+				print("okay")
+				sys.exit()
+	except IndexError:
+		pass
+station = sys.argv[1]
+makesure(station)
 first = '''const electron = require('electron');
 const app = electron.app;
 const BrowserWindow = electron.BrowserWindow;
@@ -12,10 +40,6 @@ function createWindow () {
   mainWindow = new BrowserWindow({width: 1800, height: 1200});
 
   mainWindow.loadURL(`'''
-
-station = open(".conf", "r")
-station = station.read()
-station = station.split()[0]
 
 second = '''`);
 
@@ -53,5 +77,4 @@ file = open("index.js", "w")
 total = f"{first}{station}{second}"
 file.write(''.join(total))
 file.close()
-
 os.system("electron index.js")
