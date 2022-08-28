0eea696a01d5cb0a4b0cac013fcab48d42089797
NacreousDawn596 <75474839+NacreousDawn596@users.noreply.github.com>
2022-08-28 14:05:01 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,36 +1,12 @@
 import os
 import sys
 from requests import *
-from bs4 import *
 def makesure(link):
 	if get(link).status_code == 200:
-		security(link)
+		pass
 	else:
 		print("link incorrect")
 		sys.exit()
-def security(ty):
-	result = BeautifulSoup(os.popen(f"curl -s http://trueurl.net/service?q={ty.replace(':', '%3A').replace('/', '%2F')}&lucky=on&Uncloak=Find+True+URL").read(), 'html.parser').find_all('td')[3].find_all('a')
-	try:
-		if len(result) == 1:
-			result = result[0].get('href')
-		else:
-			result = result[1].get('href')
-		if ty == result:
-			pass
-		else:
-			umm = input(f"do you want to be redirected to this link: '{result}'? y/n\n->")
-			if "y" in umm.lower():
-				pass
-			else:
-				print("okay")
-				sys.exit()
-	except IndexError:
-		humm = input(f'is this domain trusted? "{ty.replace("https://", "").replace("http://", "").split("/")[0]}" y/n\n->')
-		if "y" in humm.lower():
-			pass
-		else:
-			print("okay")
-			sys.exit()
 
 try:
 	station = sys.argv[1]
